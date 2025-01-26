e740410a69653f0d8cdc6aad7efa9bbbef931b97
Pamela Ocampo <pamela.ocampo@gmail.com>
2025-01-25 21:50:41 -0800

    Sorts sponsors by level

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -3,6 +3,7 @@ import { urlFor, SanityImageSource, client } from '@/lib/sanity';
 import { Card } from '@/components/Card';
 import { getColorClasses } from '@/utils/color';
 import { notFound } from 'next/navigation';
+import { sponsorLevelOrder } from '@/types/sponsor';
 
 interface Sponsor {
   _id: string;
@@ -10,27 +11,44 @@ interface Sponsor {
   website: string;
   logo: SanityImageSource;
   description: string;
+  sponsorshipLevel: string
 }
 
 async function getSponsors(): Promise<Sponsor[]> {
   return client.fetch(`
-      *[_type == "sponsor"] | order(name asc) {
+      *[_type == "sponsor"] {
         _id,
         name,
         website,
         logo,
-        description
+        description,
+        sponsorshipLevel
       }
     `);
+}
+
+function sortSponsors(sponsors: Sponsor[]): Sponsor[] {
+  return sponsors.sort((a, b) => {
+    if (a.sponsorshipLevel && b.sponsorshipLevel) {
+      const levelA = a.sponsorshipLevel.toLowerCase();
+      const levelB = b.sponsorshipLevel.toLowerCase();
+      const orderA = sponsorLevelOrder[levelA as keyof typeof sponsorLevelOrder];
+      const orderB = sponsorLevelOrder[levelB as keyof typeof sponsorLevelOrder];
+
+      return orderA - orderB;
+    }
+    return a.name.localeCompare(b.name);
+  });
 }
 
 export default async function SponsorsPage() {
   const sponsors: Sponsor[] = await getSponsors();
   if (!sponsors) notFound();
+  const sortedSponsors = sortSponsors(sponsors);
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-      {sponsors?.map((sponsor, index) => {
+      {sortedSponsors?.map((sponsor, index) => {
         const colorClasses = getColorClasses(index);
         return (
           <Card key={sponsor._id} colorClasses={colorClasses}>
@@ -41,7 +59,7 @@ export default async function SponsorsPage() {
               className="block w-full h-48 mb-4 overflow-hidden"
             >
               <Image
-                src={urlFor(sponsor.logo).width(600).height(400).url()}
+                src={urlFor(sponsor.logo).url()}
                 alt={sponsor.name}
                 fill
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
---
+++ src/types/sponsor.ts
@@ -0,0 +1,18 @@
+
+export const SponsorLevels = {
+	PLATINUM: 'platinum',
+	GOLD: 'gold',
+	SILVER: 'silver',
+	BRONZE: 'bronze',
+} as const;
+
+
+export type SponsorLevel = typeof SponsorLevels[keyof typeof SponsorLevels];
+export const sponsorLevelOrder: Record<SponsorLevel, number> = {
+	[SponsorLevels.PLATINUM]: 1,
+	[SponsorLevels.GOLD]: 2,
+	[SponsorLevels.SILVER]: 3,
+	[SponsorLevels.BRONZE]: 4,
+};
+
+
