075dc9ec6e012c780d3c73993c855f48ec0de6ea
Pamela Ocampo <pamela.ocampo@gmail.com>
2025-02-16 15:22:16 -0800

    Linting

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -7,33 +7,32 @@ const slackChannelId = process.env.SLACK_CHANNEL_ID;
 const slackClient = new WebClient(slackToken);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-	if (req.method === 'POST') {
-		const { name, email, message } = req.body;
-		const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(
-			'Hello from Eclipse Racing')}&body=${encodeURIComponent(
-				`Hi ${name},\n\nThank you for your message!\n\n">> ${message}"`
-		)}`;
+  if (req.method === 'POST') {
+    const { name, email, message } = req.body;
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(
+      'Hello from Eclipse Racing'
+    )}&body=${encodeURIComponent(`Hi ${name},\n\nThank you for your message!\n\n">> ${message}"`)}`;
 
-		try {
-			await slackClient.chat.postMessage({
-				channel: slackChannelId!,
-				text: `Message from ${name} (${email}): ${message}`,
-				blocks: [
-					{
-						type: 'section',
-						text: {
-							type: 'mrkdwn',
-							text: `*Message from ${name} (<mailto:${email}|${email}>):*\n\n${message}\n\n[${mailtoLink}|Reply via Email>]`,
-						},
-					},
-				],
-			});
-			res.status(200).json({ success: true });
-		} catch (error) {
-			console.error(error);
-			res.status(500).json({ success: false });
-		}
-	} else {
-		res.status(405).json({ success: false });
-	}
+    try {
+      await slackClient.chat.postMessage({
+        channel: slackChannelId!,
+        text: `Message from ${name} (${email}): ${message}`,
+        blocks: [
+          {
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `*Message from ${name} (<mailto:${email}|${email}>):*\n\n${message}\n\n[${mailtoLink}|Reply via Email>]`,
+            },
+          },
+        ],
+      });
+      res.status(200).json({ success: true });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false });
+    }
+  } else {
+    res.status(405).json({ success: false });
+  }
 }
